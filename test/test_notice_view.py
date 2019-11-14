345cca04626c0e3b19e63db7a3366aa8ae48f965
Qingyu Deng <bitdqy@hotmail.com>
2019-11-14 18:48:25 +0800

    feat: add post view

--- test/test_notice_view.py
+++ test/test_notice_view.py
@@ -1,20 +1,50 @@
 from unittest.mock import MagicMock
 
 from flask import Flask
+import pytest
 
 from ytb_pubsubhubbub.notice_view import YouTubeNoticeView
 
 
-def test_intent_verification():
-    intent_validator = MagicMock()
-
-    YouTubeNoticeView.set_handler(intent_validator=intent_validator)
-
+@pytest.fixture
+def client():
     app = Flask(__name__)
     app.add_url_rule("/notice", view_func=YouTubeNoticeView.as_view("notice_view"))
 
-    client = app.test_client()
-    client.get(
+    class TestClient:
+        def __init__(self):
+            self._client = app.test_client()
+
+        def simulate_intent_verification(self):
+            """
+            Spec of Intent Verification refers to
+            http://pubsubhubbub.github.io/PubSubHubbub/pubsubhubbub-core-0.4.html#verifysub
+            """
+            return self._client.get(
+                "/notice",
+                query_string={
+                    "hub.mode": "subscribe",
+                    "hub.topic": "https://www.youtube.com/xml/feeds/videos.xml?channel_id=channel_id",
+                    "hub.challenge": "1234",
+                    "hub.lease_seconds": 123,
+                },
+            )
+
+        def simulate_content_distribution(self):
+            """
+            Spec of Content Distribution refers to
+            http://pubsubhubbub.github.io/PubSubHubbub/pubsubhubbub-core-0.4.html#contentdistribution
+
+            NOTE: the Link header is not simulated here
+            """
+            return self._client.post("/notice", data="content")
+
+    return TestClient()
+
+
+def simulate_intent_verification(client):
+
+    return client.get(
         "/notice",
         query_string={
             "hub.mode": "subscribe",
@@ -24,5 +54,54 @@ def test_intent_verification():
         },
     )
 
-    intent_validator.assert_called_with(channel_id="channel_id", mode="subscribe")
 
+def test_intent_verification(client):
+
+    intent_validator = MagicMock(return_value=True)
+    subscription_handler = MagicMock()
+    YouTubeNoticeView.set_handler(
+        intent_validator=intent_validator, subscription_handler=subscription_handler
+    )
+
+    resp = client.simulate_intent_verification()
+
+    intent_validator.assert_called_with(channel_id="channel_id", mode="subscribe")
+    subscription_handler.assert_called_with(
+        channel_id="channel_id", mode="subscribe", lease_seconds=123
+    )
+
+    def assert_intent_confirmed():
+        assert resp.status_code == 200
+        assert resp.data == b"1234"
+
+    assert_intent_confirmed()
+
+
+def test_intent_verification_rejected(client):
+
+    intent_validator = MagicMock(return_value=False)
+    subscription_handler = MagicMock()
+    YouTubeNoticeView.set_handler(
+        intent_validator=intent_validator, subscription_handler=subscription_handler
+    )
+
+    resp = client.simulate_intent_verification()
+
+    intent_validator.assert_called_with(channel_id="channel_id", mode="subscribe")
+    subscription_handler.assert_not_called()
+
+    def assert_intent_rejected():
+        assert resp.status_code == 400
+
+    assert_intent_rejected()
+
+
+def test_content_distribution(client):
+    notice_handler = MagicMock()
+
+    YouTubeNoticeView.set_handler(notice_handler=notice_handler)
+
+    resp = client.simulate_content_distribution()
+    notice_handler.assert_called_once_with("content")
+
+    assert resp.status_code == 200
--- ytb_pubsubhubbub/notice_view.py
+++ ytb_pubsubhubbub/notice_view.py
@@ -7,14 +7,20 @@ from flask import request
 class YouTubeNoticeView(MethodView):
 
     intent_validator = lambda *args: True
+    subscription_handler = None
+    notice_handler = None
     """
     Expected function signature
     intent_validator(channel_id: str, mode: get_intent_mode) -> bool
     """
 
     @classmethod
-    def set_handler(cls, intent_validator):
+    def set_handler(
+        cls, intent_validator=None, subscription_handler=None, notice_handler=None
+    ):
         cls.intent_validator = intent_validator
+        cls.subscription_handler = subscription_handler
+        cls.notice_handler = notice_handler
 
     def get(self):
         """GET method implements the intent verification of Subscriber
@@ -30,4 +36,33 @@ class YouTubeNoticeView(MethodView):
         def get_intent_mode():
             return request.args["hub.mode"]
 
-        self.intent_validator(channel_id=extract_channel_id(), mode=get_intent_mode())
+        def get_lease_seconds():
+            return int(request.args["hub.lease_seconds"])
+
+        def confirm_intention():
+            challenge = request.args["hub.challenge"]
+            return challenge, 200
+
+        def reject_intention():
+            return "", 400
+
+        if self.intent_validator(
+            channel_id=extract_channel_id(), mode=get_intent_mode()
+        ):
+            self.subscription_handler(
+                channel_id=extract_channel_id(),
+                mode=get_intent_mode(),
+                lease_seconds=get_lease_seconds(),
+            )
+
+            return confirm_intention()
+
+        return reject_intention()
+
+    def post(self):
+        self.notice_handler(request.data.decode("utf-8"))
+
+        def response_with_success():
+            return "", 200
+
+        return response_with_success()
